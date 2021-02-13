d3b8ab7b924828f67239f0d2fb7deb7ddd523319
Renan Castro <renandf@gmail.com>
2021-02-13 11:40:29 +1100

    feat: Create Header with unstyled Nav

--- components/Page.js
+++ components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h1>Page</h1>
       {children}
     </div>
