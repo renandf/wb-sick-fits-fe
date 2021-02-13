0d010958913157003926cf4398603192d650eaa7
Renan Castro <renandf@gmail.com>
2021-02-14 09:58:53 +1100

    feat: Implement NProgress
    NProgress shows a loading bar at the top of the page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import { ApolloProvider } from '@apollo/client';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import PropTypes from 'prop-types';
 import Page from '../components/Page';
 import withData from '../lib/withData';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo }) {
   return (
