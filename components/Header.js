15720724267552a5ba119cd73c0fef8a4f5e8a99
Renan Castro <renandf@gmail.com>
2021-02-27 15:00:57 +1100

    feat: Adjust Header+Nav styles

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
     border-bottom: 10px solid var(--black, black);
   }
 
@@ -40,11 +40,11 @@ export default function Header() {
         <Logo>
           <Link href="/">Sick fits</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
